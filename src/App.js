dd57e0d9b4d14f7ceba2296e8a362f4866b54986
celicasti9 <3616@holbertonschool.com>
2021-09-29 22:27:58 +0000

    Editing body

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <h1>Bienvenido a un mundo de codigo virtual infinito!</h1>
 	  <body>
-          <p> Esta pagina ha sido creada con la intencion de practicar el desarrollo front-end de varias aplicaciones </p>
+          <p> Esta pagina ha sido creada con la intencion de practicar el desarrollo y adaptacion de aplicaciones web </p>
           </body>
 	  <a
           className="App-link"
